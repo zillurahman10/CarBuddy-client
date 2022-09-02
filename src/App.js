a350c0eff5c3bfb4a40bea9f176ab6ab30fee1b4
zilurahman10 <raihan.brac2019@gmail.com>
2022-09-02 15:34:24 +0600

    transfered

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home'
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
